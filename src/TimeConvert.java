c95f8928c14a7a24a09264be2a72cf8ffd2c89c8
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-02-17 20:47:30 +0200

    Initial commit

---
+++ src/TimeConvert.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TimeConvert {
+
+    public static void main(String[] Strings) {
+
+
+        double minutesInYear = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите количество минут: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " минут примерно " + years + " лет и " + days + " дней");
+    }
+}
