43eb8220992614afc9d61273d4872bf535ea7ac2
AlexLevanchenko <99507725+AlexLevanchenko@users.noreply.github.com>
2022-02-18 10:49:04 +0200

    Update TimeConvert.java

--- src/TimeConvert.java
+++ src/TimeConvert.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
+
 public class TimeConvert {
 
     public static void main(String[] Strings) {
 
-
-        double minutesInYear = 60 * 24 * 365;
-
+        final int minInHour = 60;
+        final int hourInDay = 24;
+        final int daysInYear = 365;
+        double minutesInYear = minInHour * hourInDay * daysInYear;
         Scanner input = new Scanner(System.in);
-
         System.out.print("Введите количество минут: ");
-
         double min = input.nextDouble();
-
         long years = (long) (min / minutesInYear);
-        int days = (int) (min / 60 / 24) % 365;
-
+        int days = (int) (min / minInHour / hourInDay) % daysInYear;
         System.out.println((int) min + " минут это примерно " + years + " лет и " + days + " дней");
     }
 }
